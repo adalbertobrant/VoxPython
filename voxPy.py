261b596f046cb2887556867409f34f9e31a07353
Adalberto Caldeira Brant Filho <adalbertobrant@gmail.com>
2018-05-20 02:27:00 -0300

    Create voxPy.py

---
+++ voxPy.py
@@ -0,0 +1,8 @@
+## Script idealizado por Adalberto Caldeira
+## O objetivo do script é automatizar o processo da lista de nomes de pacientes utilizado no consultório , para que o paciente
+## receba um audio individualizado e personalizado para o seu tratamento em campanhas de Marketing Neuromental Pica das Galáxias.
+## Usando a metodologia T.P.G e T.A.B.C
+
+
+
+
